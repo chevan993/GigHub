5f8bf3fd0dc7e3289e9cf0e19eb347a895116095
Filip Lukačevićhfsdlkfj <hadihafis@hotmail.com>
2018-07-30 11:35:31 +0000

    Implement My Upcoming Gigs
    git-tfs-id: [filiplukacevic.visualstudio.com]$/GigHub/GigHub;C27

---
+++ GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,30 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var following = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(a => a.Followee)
+                .ToList();
+
+            return View(following);
+        }
+    }
+}
