99ab7d4afcc09800a8dedf69c4da747b97c7b69b
Filip Lukačevićhfsdlkfj <hadihafis@hotmail.com>
2018-08-03 14:05:42 +0000

    Add Gig Details Page
    git-tfs-id: [filiplukacevic.visualstudio.com]$/GigHub/GigHub;C38

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -59,7 +59,7 @@ namespace GigHub.Controllers
                 Genres = _context.Genres.ToList(),
                 Heading = "Add a Gig"
             };
-            return View(viewModel);
+            return View("GigForm", viewModel);
         }
 
         [Authorize]
@@ -131,6 +131,31 @@ namespace GigHub.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Mine", "Gigs");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var gig = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .SingleOrDefault(g => g.Id == id);
+
+            if(gig == null) { return HttpNotFound(); }
+
+            var gigDetailsViewModel = new GigDetailsViewModel { Gig = gig };
+
+            if(User.Identity.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+
+                gigDetailsViewModel.IsAttending = _context.Attendances
+                    .Any(a => a.GigId == gig.Id && a.AttendeeId == userId);
+
+                gigDetailsViewModel.IsFollowing = _context.Followings
+                    .Any(f => f.FolloweeId == gig.ArtistId && f.FollowerId == userId);
+            }
+
+            return View(gigDetailsViewModel);
         }
     }
 }
