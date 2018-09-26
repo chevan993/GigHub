15d531c6328748212a4ddfd0d0d2d25e6d129778
Filip <hadihafis@hotmail.com>
2018-09-27 00:33:19 +0200

    Refactor GigsController

--- GigHub/Repositories/AttendanceRepository.cs
+++ GigHub/Repositories/AttendanceRepository.cs
@@ -20,5 +20,12 @@ namespace GigHub.Repositories
                 .Where(a => a.AttendeeId == userId && a.Gig.DateTime > DateTime.Now)
                 .ToList();
         }
+
+        public Attendance GetAttendance(int gigId, string userId)
+        {
+            return _context
+                .Attendances
+                .Single(a => a.GigId == gigId && a.AttendeeId == userId);
+        }
     }
 }
--- GigHub/Repositories/GigRepository.cs
+++ GigHub/Repositories/GigRepository.cs
@@ -32,5 +32,27 @@ namespace GigHub.Repositories
                 .Include(g => g.Attendances.Select(a => a.Attendee))
                 .SingleOrDefault(g => g.Id == gigId);
         }
+
+        public Gig GetGigById(int gigId)
+        {
+            return _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .SingleOrDefault(g => g.Id == gigId);
+        }
+
+        public IEnumerable<Gig> GetUpcomingGigsByArtist(string userId)
+        {
+            return _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now && !g.IsCanceled)
+                .Include(g => g.Genre)
+                .ToList();
+        }
+
+        public void AddGig(Gig gig)
+        {
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+        }
     }
 }
